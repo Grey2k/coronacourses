4c6da0f49b018f3f8c3953d76d8a29e8029a175c
Marko Semet <marko10_000@gmx.de>
2020-03-21 11:20:56 +0100

    Add django rest framework

--- api_backend/setup.py
+++ api_backend/setup.py
@@ -7,5 +7,10 @@ setup(name="coronacourses_api",
       description="API backend for CoronaCourses",
       url="https://github.com/jjegg01/coronacourses",
       packages=find_packages("coronacourses_api"),
-      install_requires=["django>=3.0<4"]
+      install_requires=["django>=3.0<4",
+                        "djangorestframework>=3.11.0<4",
+                        "markdown>=3.2.1<4", # Support for doc
+                        "coreapi>=2.3.3<4",
+                        "Pygments>=2.6.1<3"
+      ]
 )
